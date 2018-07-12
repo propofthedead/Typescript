bd254a0979b006291b0ab8fcc0a1f55341dd77d8
Foster Stule <camodude5@yahoo.com>
2018-07-12 14:26:07 -0400

    moved to a different folder

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+var a = 0;
+var b = "ABC";
+function add1(num) {
+    console.log(num + 1);
+}
+function log(str) {
+    console.log(str);
+}
+add1(a);
+log("the vale of b is " + b);
--- index.js
+++
@@ -1,4 +0,0 @@
-var a = 0;
-function add1(num) {
-    console.log(num + 1);
-}
