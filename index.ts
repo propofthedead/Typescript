4f7c3c5f286c1768349741ce2da3cc821f8b5059
Foster Stule <camodude5@yahoo.com>
2018-07-12 14:26:34 -0400

    improved

--- index.ts
+++ index.ts
@@ -1,4 +1,10 @@
 let a: number=0;
+let b:string="ABC";
  function add1(num : number) : void{
      console.log(num+1);
  }
+function log(str: string): void{
+    console.log(str);
+}
+ add1(a);
+ log(`the vale of b is ${b}`);
